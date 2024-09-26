a3aeb39f07b7dceabc315541803c0cf98579e364
ka8893 <a8893@n-koei.co.jp>
2024-09-26 17:51:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if o_api_key or g_api_key:
     if mode == "OpenAI GPT-4o":
         st.markdown('### OpenAI GPT-4o')
         uploaded_file = st.file_uploader(
-            "Upload an image to analyze", type=["jpg", "jpeg", "png"]
+            "Upload an image to analyze", type=["jpg", "jpeg", "png"], accept_multiple_files=True
         )
         uploaded_file2 = st.file_uploader(
             "Upload an image to analyze", type=["jpg", "jpeg", "png"]
