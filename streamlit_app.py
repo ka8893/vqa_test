0f5fdb5702f98fc2fdcec357bb9a2133f175bdc4
ka8893 <a8893@n-koei.co.jp>
2024-09-26 15:10:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,20 @@
 import streamlit as st
-st.markdown('### OpenAI GPT-4o')
+st.markdown('### Google Gemini 1.5 Flash')
 uploaded_file = st.file_uploader("画像をアップロードしてください。")
+
+!pip install -q -U google-generativeai
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+# Used to securely store your API key
+from google.colab import userdata
+from IPython.display import display
+from IPython.display import Markdown
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+genai.configure(api_key="AIzaSyCBmmgXXzI2nLM4PAFQ2V0DI95F0iyPflI")
