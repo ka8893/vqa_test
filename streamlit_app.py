ca05a3474c355575c3c95b447ebdfd8b624d4591
ka8893 <a8893@n-koei.co.jp>
2024-09-26 17:10:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,6 @@ import pathlib
 import textwrap
 import google.generativeai as genai
 
-from IPython.display import display
-from IPython.display import Markdown
-
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
