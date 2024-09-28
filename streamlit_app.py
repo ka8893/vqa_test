03bd51af6d228421a82407b2da1a4bd5333e2c64
ka8893 <a8893@n-koei.co.jp>
2024-09-28 16:23:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,10 +153,11 @@ if U > 0:
         # モデルの準備
         model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest", safety_settings=safety_settings)
         if uploaded_file:
-            st.image(uploaded_file)
+            image = PIL_Image.open(uploaded_file)
+            st.image(image)
             response = model.generate_content([
                 "起こっているのは、火災、大雪、冠水、地震、倒木、電柱倒壊、非該当のうちどれか一言で教えてください。",
-                uploaded_file
+                image
             ], stream=True)
             response.resolve()
  
