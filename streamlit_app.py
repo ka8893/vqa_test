c20735ef91ebd2ead3d3c09df0b392351d22fd24
ka8893 <a8893@n-koei.co.jp>
2024-09-28 14:02:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,10 +60,6 @@ if U > 0:
         st.markdown('### OpenAI GPT-4o')
         uploaded_file = st.file_uploader(
             "Upload an image to analyze", type=["jpg", "jpeg", "png"]
-            # , accept_multiple_files=True
-        )
-        uploaded_file2 = st.file_uploader(
-            "Upload an image to analyze", type=["jpg", "jpeg", "png"]
         )
         base_prompt = "起こっているのは、火災、大雪、冠水、増水、土砂崩れ、落石、電柱倒壊、非該当のうちどれか一言で教えてください。"
         input_image_prompt = st.text_area(
