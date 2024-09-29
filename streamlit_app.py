394580ed2bdd5b9a470b5ebd3345132e02301f6f
ka8893 <a8893@n-koei.co.jp>
2024-09-29 13:11:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,49 +19,6 @@ genai.configure(api_key=st.secrets.GOOGLEAPI.google_api_key)
 openai.api_key = st.secrets.OPENAIAPI.openai_api_key
 # openai.api_key = st.secrets["OPENAIAPI"]["openai_api_key"]
 
-import torch
-from transformers import LlamaTokenizer, AutoModelForVision2Seq, BlipImageProcessor
-from PIL import Image
-import requests
-
-# helper function to format input prompts
-def build_prompt(prompt="", sep="\n\n### "):
-    sys_msg = "以下は、タスクを説明する指示と、文脈のある入力の組み合わせです。要求を適切に満たす応答を書きなさい。"
-    p = sys_msg
-    roles = ["指示", "応答"]
-    user_query = "与えられた画像について、詳細に述べてください。"
-    msgs = [": \n" + user_query, ": "]
-    if prompt:
-        roles.insert(1, "入力")
-        msgs.insert(1, ": \n" + prompt)
-    for role, msg in zip(roles, msgs):
-        p += sep + role + msg
-    return p
-
-def image_config():
-    col1, col2 = st.columns(2)
-
-    with col1:
-        size = st.selectbox("Size", options=["256x256", "512x512", "1024x1024"])
-    with col2:
-        num = st.number_input("Number of generation", step=1, min_value=1, max_value=5)
-
-    if size == "256x256":
-        height = 256
-        width = 256
-    elif size == "512x512":
-        height = 512
-        width = 512
-    elif size == "1024x1024":
-        height = 1024
-        width = 1024
-
-    return num, height, width
-
-
-if "all_text" not in st.session_state:
-    st.session_state.all_text = []
-
 U = 1
 with st.sidebar:
     st.title("VQA Model")
@@ -193,36 +150,5 @@ if U > 0:
                 # レスポンスの表示
                 for response in responses:
                     st.write(response.text)
-    """
-    if mode == "Japanese InstructBLIP Alpha":
-        # load model
-        model = AutoModelForVision2Seq.from_pretrained("stabilityai/japanese-instructblip-alpha", trust_remote_code=True)
-        processor = BlipImageProcessor.from_pretrained("stabilityai/japanese-instructblip-alpha")
-        tokenizer = LlamaTokenizer.from_pretrained("novelai/nerdstash-tokenizer-v1", additional_special_tokens=['▁▁'])
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        model.to(device)
-        
-        # prepare inputs
-        url = "https://images.unsplash.com/photo-1582538885592-e70a5d7ab3d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
-        image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
-        prompt = "" # input empty string for image captioning. You can also input questions as prompts 
-        prompt = build_prompt(prompt)
-        inputs = processor(images=image, return_tensors="pt")
-        text_encoding = tokenizer(prompt, add_special_tokens=False, return_tensors="pt")
-        text_encoding["qformer_input_ids"] = text_encoding["input_ids"].clone()
-        text_encoding["qformer_attention_mask"] = text_encoding["attention_mask"].clone()
-        inputs.update(text_encoding)
-        
-        # generate
-        outputs = model.generate(
-            **inputs.to(device, dtype=model.dtype),
-            num_beams=5,
-            max_new_tokens=32,
-            min_length=1,
-        )
-        generated_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-        print(generated_text)
-        # 桜と東京スカイツリー
-    """
 else:
     st.info("OPENAI_API_KEY")
